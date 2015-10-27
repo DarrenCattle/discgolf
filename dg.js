1f002d3b63160fdead3e16e1d13f4f7e96be1008
darrencattle <cattle.darren@gmail.com>
2015-10-27 00:37:43 -0500

    working
    now need db

--- dg.js
+++ dg.js
@@ -5,42 +5,44 @@
 var database = {};
 var coursedata = {};
 
-database.put("dcattle", null);
-
-coursedata.put("Cat Hollow", {
+coursedata["Cat Hollow"] = {
 	scoresheet: [3,3,3,3,4,3,3,3,3,4,3,3,3,3,3,3,3,3],
 	holelength: [300,300,300,300,400,300,300,300,300,400,300,300,300,300,300,300,300,300],
 	location: "Round Rock",
 	picture: "http://imgur.com/cathollow"
-});
-displayCourse("Cat Hollow");
+};
+console.log(displayCourse("Cat Hollow"));
+console.log(storeRound(["dcattle", "ridhoq"], "Cat Hollow", 
+	[
+		[2,2,3,3,4,3,3,3,3,4,1,6,3,3,3,2,3,3],
+		[3,3,3,3,4,5,3,2,3,4,3,3,3,3,7,3,3,3]
+	]
+));
 
 function storeRound(name, coursename, score) {
 
-	if(name.length() != score.length() || name==null || score==null) {
+	if(name.length != score.length || name==null || score==null) {
 		return "invalid";
 	}
-	if(name.length() == 1) {
+	if(name.length == 1) {
 		var schema = {
-			player: name,
+			player: name[0],
 			course: coursename,
-			//date: datetime.getDate();
-			scoresheet: score
+			scoresheet: score[0]
 		};
-		database.push(schema);
+		database[name] = schema;
 		return displayRound(schema);
 	}
 	else {
 		var multi = "";
 		for(var a=0; a<name.length; a++) {
 			var schema = {
-				player: name,
+				player: name[a],
 				course: coursename,
-				//date: datetime.getDate();
-				scoresheet: score
+				scoresheet: score[a]
 			};
-			database.push(schema);
-			multi += displayRound(schema);
+			database[name] = schema;
+			multi += displayRound(schema) + "\n";
 		}
 		return multi;
 	}
@@ -49,24 +51,37 @@ function storeRound(name, coursename, score) {
 
 function displayRound(round) {
 	var retstring;
+	var myscore = sum(round.scoresheet);
+	var parscore = sum(coursedata[round.course].scoresheet);
+	var overunder = myscore-parscore > 0 ? "+" : "";
 	retstring = 
 		"Player: " + round.player + "\n" +
-		"Course: " + round.coursename + "\n" +
-		"Score: " + round.scoresheet.sum() + "\n" +
-		"Par: " + coursedata.get(round.coursename).scoresheet.sum() + "\n";
+		"Course: " + round.course + "\n" +
+		"Hole Scores: " + round.scoresheet + "\n" +
+		"Par: " + parscore + "\n" +
+		"Total Score: " + myscore + "\n" +
+		"Final Score: " + overunder + (myscore-parscore) + "\n";
 	return retstring;
 };
 
 function displayCourse(coursename) {
 	var retstring;
-	var course = coursedata.get(coursename);
+	var course = coursedata[coursename];
 	retstring = 
 		"Course Name: " + coursename + "\n" +
 		"Hole Scores: " + course.scoresheet + "\n" +
-		"Par: " + course.scoresheet.sum() + "\n" +
+		"Par: " + sum(course.scoresheet) + "\n" +
 		"Hole Lengths: " + course.holelength + "\n" +
-		"Total Distance: " + course.holelength.sum() + "\n" +
+		"Total Distance: " + sum(course.holelength) + "\n" +
 		"Location: " + course.location + "\n" +
 		"Course Map: " + course.picture + "\n";
 	return retstring;
 };
+
+function sum(array) {
+	var total = 0;
+	for (var a in array) {
+		total+=array[a];
+	}
+	return total;
+}
