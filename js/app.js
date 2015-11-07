7d194f7a621049b622875cefb298c6b39c1d8dd5
darrencattle <cattle.darren@gmail.com>
2015-11-07 12:34:23 -0600

    some UI design
    angularrr

--- js/app.js
+++ js/app.js
@@ -49,15 +49,55 @@
 		location: 'Round Rock',
 		picture: 'http://imgur.com/cathollow'
 	};
+	coursedata["Fat Hollow"] = {
+		holes: {
+			hole1: {	par: 3,
+						length: 240	},
+			hole2: {	par: 3,
+						length: 186	},
+			hole3: {	par: 3,
+						length: 291	},
+			hole4: {	par: 3,
+						length: 375	},
+			hole5: {	par: 4,
+						length: 585	},
+			hole6: {	par: 3,
+						length: 231	},
+			hole7: {	par: 3,
+						length: 216	},
+			hole8: {	par: 3,
+						length: 312	},
+			hole9: {	par: 3,
+						length: 219	},
+			hole10: {	par: 4,
+						length: 498	},
+			hole11: {	par: 3,
+						length: 393	},
+			hole12: {	par: 3,
+						length: 321	},
+			hole13: {	par: 3,
+						length: 228	},
+			hole14: {	par: 3,
+						length: 213	},
+			hole15: {	par: 3,
+						length: 204	},
+			hole16: {	par: 3,
+						length: 207	},
+			hole17: {	par: 3,
+						length: 297	},
+			hole18: {	par: 3,
+						length: 222	}},
+		location: 'Round Rock',
+		picture: 'http://imgur.com/fathollow'
+	};
 	console.log(coursedata);
 
 	//main array controller with calculate and update functions for graph and table
 	app.controller('dgController', function() {
 
-		this.course = "Cat Hollow"
+		this.course = "Cat Hollow";
 		this.holes = coursedata[this.course].holes;
 		console.log(this.holes);
-		console.log('update');
 
 		//push new information through the controller
 		this.update = function () {
@@ -66,7 +106,7 @@
 			console.log('update');
 		};
 		//calculate solar angles using empirical formulae
-		this.calculate = function (latitude, day) {
+		this.calculate = function (score) {
 			this.update();
 		};
 	});
